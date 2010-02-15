79cf059af523b6e6fd1aa10ac43f5703519311a5
jolo <jolo@28dda339-3e7f-0410-9691-cab309f6cb01>
2010-02-15 08:54:38 +0000

    added database actions like begin(), commit(), rollback() to StandardItemModel. Additionally some minor refactorings

--- branches/with-libpq-backend/src/models/StandardItemModel.h
+++ branches/with-libpq-backend/src/models/StandardItemModel.h
@@ -10,6 +10,12 @@
 
 #include <QStandardItemModel>
 
+class QWidget;
+
+class DataManager;
+class Postgres;
+class ProfileLoggerDatabase;
+
 class StandardItemModel : public QStandardItemModel {
     Q_OBJECT
 
@@ -17,8 +23,35 @@ public:
     StandardItemModel(QObject* p = 0);
     virtual ~StandardItemModel();
 
+    void setDataManager(DataManager* m) {
+      _dataManager = m;
+    }
+
+    DataManager* getDataManager() {
+      return _dataManager;
+    }
+
+    Postgres* getPostgres() {
+      return _postgres;
+    }
+
+    ProfileLoggerDatabase* getProfileLoggerDatabase() {
+      return _profileLoggerDatabase;
+    }
+
+    QWidget* getDialogParent();
+
+    void begin();
+    void commit();
+    void rollback();
+
 signals:
     void reloaded();
+
+ private:    
+    DataManager* _dataManager;
+    Postgres* _postgres;
+    ProfileLoggerDatabase* _profileLoggerDatabase;
 };
 
 #endif	/* _STANDARDITEMMODEL_H */
