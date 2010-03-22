b8f04b3b13298ecfd79027aa40246179a71cb080
jolo <jolo@28dda339-3e7f-0410-9691-cab309f6cb01>
2010-03-22 14:01:53 +0000

    removed obsolete file

--- branches/experimental/py/Gui/ItemViews/PenStyleItemView.py
+++
@@ -1,8 +0,0 @@
-from DataSelectionComboBox import DataSelectionComboBox
-
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-
-class PenStyleItemView(DataSelectionComboBox):
-    def __init__(self, parent, model):
-        DataManagementItemView.__init__(self, parent, model)
