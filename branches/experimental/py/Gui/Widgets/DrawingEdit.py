ae208b6526ae5acb4cf2fd7ebb57d812c11ecc1d
jolo <jolo@28dda339-3e7f-0410-9691-cab309f6cb01>
2010-03-24 08:48:35 +0000

    saving gui space

--- branches/experimental/py/Gui/Widgets/DrawingEdit.py
+++ branches/experimental/py/Gui/Widgets/DrawingEdit.py
@@ -19,75 +19,75 @@ class DrawingEdit(QSplitter):
         self.setOrientation(Qt.Horizontal)
         self.canvasS = CanvasScene(self)
         self.canvasV = CanvasView(self)
-        self.canvasV.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.canvasV.setScene(self.canvasS)
         self.configureTools()
         self.addWidget(self.toolsW)
         self.addWidget(self.canvasV)
     def configureTools(self):
         self.toolsW = QWidget(self)
-        self.toolsW.setLayout(QVBoxLayout(self.toolsW))
-        self.toolsW.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.toolsW.setLayout(QHBoxLayout(self.toolsW))
 
         self.setupToolsWidget()
-        self.setupSettingsWidget()
 
-        self.modeW = QLabel(self.tr("No Tool Selected"), self.toolsW)
-        self.modeW.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
-        
-        self.toolsW.layout().addWidget(self.toolBoxW)
-        self.toolsW.layout().addWidget(self.settingsW)
-        self.toolsW.layout().addWidget(self.modeW)
-    def onStraightLineRequest(self):
-        self.modeW.setText(self.tr("Straight Line"))
-    def onPolygonLineRequest(self):
-        self.modeW.setText(self.tr("Polygon Line"))
-    def onRectangleRequest(self):
-        self.modeW.setText(self.tr("Rectangle"))
-    def onEllipseRequest(self):
-        self.modeW.setText(self.tr("Ellipse"))
-    def onPathRequest(self):
-        self.modeW.setText(self.tr("Path"))
-    def onDeleteRequest(self):
-        self.modeW.setText(self.tr("Delete"))
-    def onMoveRequest(self):
-        self.modeW.setText(self.tr("Move"))
-    def onEditRequest(self):
-        self.modeW.setText(self.tr("Edit"))
-    def setupSettingsWidget(self):
         self.settingsW = QWidget(self.toolsW)
-        self.settingsW.setLayout(QGridLayout(self.settingsW))
+        self.settingsW.setLayout(QVBoxLayout(self.settingsW))
         self.pensW = PenItemView(self.settingsW, QApplication.instance().penModel)
         self.brushesW = BrushItemView(self.settingsW, QApplication.instance().brushModel)
-        self.settingsW.layout().addWidget(QLabel(self.tr("Pen"), self.settingsW), 0, 0)
-        self.settingsW.layout().addWidget(self.pensW, 1, 0)
-        self.settingsW.layout().addWidget(QLabel(self.tr("Brush"), self.settingsW), 0, 1)
-        self.settingsW.layout().addWidget(self.brushesW, 1, 1)
-        self.pensW.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self.brushesW.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.modeW = QLabel(self.tr("No Tool Selected"), self.settingsW)
+        self.modeW.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
+        self.settingsW.layout().addWidget(self.pensW)
+        self.settingsW.layout().addWidget(self.brushesW)
+        self.settingsW.layout().addWidget(self.modeW)
+
         self.pensW.currentDatasetChanged.connect(self.canvasS.onPenChange)
         self.brushesW.currentDatasetChanged.connect(self.canvasS.onBrushChange)
+
+        self.toolsW.layout().addWidget(self.toolBoxW)
+        self.toolsW.layout().addWidget(self.settingsW)
     def setupToolsWidget(self):
         self.toolBoxW = QWidget(self.toolsW)
-        self.toolBoxW.setLayout(QGridLayout(self.toolBoxW))
-        self.straightLineW = QPushButton(self.tr("Straight Line"), self.toolBoxW)
-        self.polygonLineW = QPushButton(self.tr("Polygon Line"), self.toolBoxW)
-        self.rectangleW = QPushButton(self.tr("Rectangle"), self.toolBoxW)
-        self.ellipseW = QPushButton(self.tr("Ellipse"), self.toolBoxW)
-        self.pathW = QPushButton(self.tr("Path"), self.toolBoxW)
-        self.deleteW = QPushButton(self.tr("Delete"), self.toolBoxW)
-        self.editW = QPushButton(self.tr("Edit"), self.toolBoxW)
-        self.moveW = QPushButton(self.tr("Move"), self.toolBoxW)
+        self.toolBoxW.setLayout(QVBoxLayout(self.toolBoxW))
+        self.straightLineW = QToolButton(self.toolBoxW)
+        self.straightLineW.setToolTip(self.tr("Straight Line"))
+        self.straightLineW.setIcon(self.createStraightLineIcon())
+
+        self.polygonLineW = QToolButton(self.toolBoxW)
+        self.polygonLineW.setToolTip(self.tr("Polygon"))
+        self.polygonLineW.setIcon(self.createPolygonLineIcon())
+
+        self.rectangleW = QToolButton(self.toolBoxW)
+        self.rectangleW.setToolTip(self.tr("Rectangle"))
+        self.rectangleW.setIcon(self.createRectangleIcon())
+
+        self.ellipseW = QToolButton(self.toolBoxW)
+        self.ellipseW.setToolTip(self.tr("Ellipse"))
+        self.ellipseW.setIcon(self.createEllipseIcon())
+
+        self.pathW = QToolButton(self.toolBoxW)
+        self.pathW.setToolTip(self.tr("Path"))
+        self.pathW.setIcon(self.createPathIcon())
+
+        self.deleteW = QToolButton(self.toolBoxW)
+        self.deleteW.setToolTip(self.tr("Delete"))
+        self.deleteW.setIcon(self.createTextIcon(self.tr("D")))
+
+        self.editW = QToolButton(self.toolBoxW)
+        self.editW.setToolTip(self.tr("Edit"))
+        self.editW.setIcon(self.createTextIcon(self.tr("E")))
+
+        self.moveW = QToolButton(self.toolBoxW)
+        self.moveW.setToolTip(self.tr("Move"))
+        self.moveW.setIcon(self.createTextIcon(self.tr("M")))
         self.moveW.setEnabled(False)
 
-        self.toolBoxW.layout().addWidget(self.straightLineW, 0, 0)
-        self.toolBoxW.layout().addWidget(self.polygonLineW, 0, 1)
-        self.toolBoxW.layout().addWidget(self.rectangleW, 1, 0)
-        self.toolBoxW.layout().addWidget(self.ellipseW, 1, 1)
-        self.toolBoxW.layout().addWidget(self.pathW, 2, 0)
-        self.toolBoxW.layout().addWidget(self.editW, 2, 1)
-        self.toolBoxW.layout().addWidget(self.moveW, 3, 0)
-        self.toolBoxW.layout().addWidget(self.deleteW, 3, 1)
+        self.toolBoxW.layout().addWidget(self.straightLineW)
+        self.toolBoxW.layout().addWidget(self.polygonLineW)
+        self.toolBoxW.layout().addWidget(self.rectangleW)
+        self.toolBoxW.layout().addWidget(self.ellipseW)
+        self.toolBoxW.layout().addWidget(self.pathW)
+        self.toolBoxW.layout().addWidget(self.editW)
+        self.toolBoxW.layout().addWidget(self.moveW)
+        self.toolBoxW.layout().addWidget(self.deleteW)
 
         self.straightLineW.clicked.connect(self.onStraightLineRequest)
         self.rectangleW.clicked.connect(self.onRectangleRequest)
@@ -118,4 +118,97 @@ class DrawingEdit(QSplitter):
             self.canvasS.addItem(PolygonItem(p))
         for p in self.drawing.painterPaths:
             self.canvasS.addItem(PainterPathItem(p))
-            
+          
+    def createEmptyIcon(self, w=20, h=20):
+        pm = QPixmap(w, h)
+        pm.fill(Qt.white)
+        return pm
+    def createIconPen(self):
+        p = QPen(Qt.black)
+        p.setWidth(1)
+        p.setStyle(Qt.SolidLine)
+        return p
+    def createStraightLineIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawLine(0, 0, 20, 20)
+        ptr.end()
+        return QIcon(pm)
+    def createPolygonLineIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        poly = QPolygonF()
+        poly.append(QPointF(0, 0))
+        poly.append(QPointF(15, 10))
+        poly.append(QPointF(20, 20))
+        poly.append(QPointF(0, 10))
+        poly.append(QPointF(5, 5))
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawPolygon(poly)
+        ptr.end()
+        return QIcon(pm)
+    def createRectangleIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawRect(5, 0, 15, 20)
+        ptr.end()
+        return QIcon(pm)
+    def createPolygonIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawEllipse(5, 0, 15, 20)
+        ptr.end()
+        return QIcon(pm)
+    def createEllipseIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawEllipse(5, 0, 15, 20)
+        ptr.end()
+        return QIcon(pm)
+    def createPathIcon(self):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        poly = QPainterPath()
+        poly.lineTo(QPointF(0, 0))
+        poly.lineTo(QPointF(20, 15))
+        poly.lineTo(QPointF(20, 0))
+        poly.lineTo(QPointF(0, 10))
+        poly.lineTo(QPointF(5, 5))
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawPath(poly)
+        ptr.end()
+        return QIcon(pm)
+    def createTextIcon(self, txt):
+        pm = self.createEmptyIcon()
+        ptr = QPainter()
+        ptr.setPen(self.createIconPen())
+        ptr.begin(pm)
+        ptr.drawText(0, 10, txt)
+        ptr.end()
+        return QIcon(pm)
+    def onStraightLineRequest(self):
+        self.modeW.setText(self.tr("Straight Line"))
+    def onPolygonLineRequest(self):
+        self.modeW.setText(self.tr("Polygon Line"))
+    def onRectangleRequest(self):
+        self.modeW.setText(self.tr("Rectangle"))
+    def onEllipseRequest(self):
+        self.modeW.setText(self.tr("Ellipse"))
+    def onPathRequest(self):
+        self.modeW.setText(self.tr("Path"))
+    def onDeleteRequest(self):
+        self.modeW.setText(self.tr("Delete"))
+    def onMoveRequest(self):
+        self.modeW.setText(self.tr("Move"))
+    def onEditRequest(self):
+        self.modeW.setText(self.tr("Edit"))
