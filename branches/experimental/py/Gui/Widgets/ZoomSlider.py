896904cb50c0fa2c0c87c09c5e94775459c2e609
jolo <jolo@28dda339-3e7f-0410-9691-cab309f6cb01>
2010-03-24 09:07:46 +0000

    increased zoom area

--- branches/experimental/py/Gui/Widgets/ZoomSlider.py
+++ branches/experimental/py/Gui/Widgets/ZoomSlider.py
@@ -3,6 +3,6 @@ from Slider import *
 class ZoomSlider(Slider):
     def __init__(self, parent):
         Slider.__init__(self, parent)
-        self.setRange(0, 200)
+        self.setRange(0, 1000)
         self.setSingleStep(1)
         self.setValue(0)
